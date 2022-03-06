21a5c211645cd950dc24eaf9a8666fc3cc7da41d
krismawati <krismawatti11@gmail.com>
2022-03-06 16:46:31 +0700

    Commit Kedua

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,46 @@ Route::get('/', function () {
 });
 
 //Routing
-Route::get('/', [PageController::class, 'home']);
+Route::get('/', [HomeController::class, 'home']);
+
+//Routing Prefix
+Route::prefix('category')->group(function(){
+    Route::get('/marbel-edu-games', function){
+        return ' Pada Halaman ini menampilkan kategori Marbel Education Games';
+    });
+Route::get('/marbel-and-friends-kids-games', function(){
+    return ' Pada Halaman ini menampilkan kategori Marbel And Friends kids games';
+});
+
+Route::get('/riri-story-books', function(){
+    return ' Pada Halaman ini menampilkan kategori Riry Story Books';
+});
+
+Route::get('/kolak-kids-song', function(){
+    return ' Pada Halaman ini menampilkan kategori Kolak Kids Songs';
+ });
+});
+
+//routing parameter
+Route::get('/news' ,function(){
+    return 'news page';
+});
+Route::get('/news/{id}', function($id){
+    return 'ID : '.$id;
+});
+
+Route::prefix('/program')->group(function () {
+    Route::get('/karir'             , function () { return ' Pada halaman ini menampilkan program karir';              });
+    Route::get('/magang'            , function () { return ' Pada halaman ini menampilkan program magang';             });
+    Route::get('/kunjungan-industri', function () { return ' Pada halaman ini menampilkan program kunjungan-industri'; });
+});
+
+// Routing untuk Halaman About Us
+Route::get('/about-us', function () { return 'about-us'; }); 
+
+//Routing Halaman Contact Us
+Route::resource('contact-us', function () { return 'resources Contact Us'; });
+
+
+
+
